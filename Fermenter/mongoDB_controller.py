37f7beda03d24841c9a132783551bf3dffcefb5e
MelcherMate <melchermate28@gmail.com>
2024-02-11 19:49:37 +0100

    button press log and load to mongoDB

--- Fermenter/mongoDB_controller.py
+++ Fermenter/mongoDB_controller.py
@@ -1,12 +1,34 @@
 from pymongo import MongoClient
+from datetime import datetime
 
 #connet to database
-client = MongoClient("mongodb://localhost:27017")
+client = MongoClient('localhost', 27017)
 
 database = client['Fermenter']
 
 log_motor = database['Motor_log']
-log_temp = database['Temperature_log']
+log_airpump = database['Airpump_log']
+
+#load the data into the Database
+class MotorData:
+    @staticmethod
+    def motor_turn_on():
+        log_data = {'button': 'Motor ON', 'timestamp': datetime.now()}
+        log_motor.insert_one(log_data)
+    @staticmethod
+    def motor_turn_on():
+        log_data = {'button': 'Motor OFF', 'timestap': datetime.now()}
+        log_motor.insert_one(log_data)
+
+class AirpumpData:
+    @staticmethod
+    def airpump_turn_on():
+        log_data = {'button': 'Airpump ON', 'timestap': datetime.now()}
+        log_airpump.insert_one(log_data)
+    @staticmethod
+    def airpump_turn_off():
+        log_data = {'button': 'Airpump OFF', 'timestap': datetime.now()}
+        log_airpump.insert_one(log_data)
 
 
 
