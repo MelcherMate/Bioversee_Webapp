02d2c452da0cbb7d56a829b4b9ee5519ef38e252
MelcherMate <melchermate28@gmail.com>
2024-02-18 22:13:07 +0000

    general update

--- Fermenter/controll_codes/air_controll.py
+++ Fermenter/controll_codes/air_controll.py
@@ -3,7 +3,11 @@ from gpiozero import OutputDevice
 airpump_pin = 16
 airpump = OutputDevice(airpump_pin)
 
+var2 = None
+
 def airpump_turn_on_off():
+    global var2
+
     if var2 is None:
         var2 = 0 
     if var2.get() == 1:
