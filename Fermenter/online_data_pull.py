934349fdd13594bb036a6178a8c88e50981b23d8
MateMelcher <melchermate28@gmail.com>
2024-02-29 16:01:38 +0100

    airpump pull py added

--- Fermenter/online_data_pull.py
+++ Fermenter/online_data_pull.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 @app.route("/path/to/your/python/endpoint", methods=["POST"])
 def handle_ajax_request():
     motor_state = request.form.get("motorState")
+    airpump_state = request.form.get("airpumpState")
     print("motorState value:", motor_state)
+    print("airpumpState value:", airpump_state)
     return "AJAX request Successfully"
 
 if __name__ == "__main__":
