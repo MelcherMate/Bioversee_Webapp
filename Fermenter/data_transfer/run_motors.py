78ca4713e33e5f987c99d9f184aa52f2dc541ffb
MateMelcher <melchermate28@gmail.com>
2024-03-06 18:30:26 +0100

    run_motors.py deleted

--- Fermenter/data_transfer/run_motors.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-def run_flask_apps():
-    subprocess.Popen(['python', 'Fermenter/data_transfer/online_data_pull.py'])
-    subprocess.Popen(['node', 'Fermenter/site/server.js'])
-    
-if __name__ == "__main__":
-    run_flask_apps()
