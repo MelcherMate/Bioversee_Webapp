bbefa0e95b828bf91e3579d088e04a00da7930a2
MelcherMate <melchermate28@gmail.com>
2024-03-15 19:47:43 +0100

    api calls

--- client/apiCalls.js
+++ client/apiCalls.js
@@ -1,41 +1,41 @@
 // Function to fetch actuator state from server
 const addActuatorState = async () => {
-    // Define the URL of the API endpoint
-    const url = '/api/v1/actuator/addactuator';
+  // Define the URL of the API endpoint
+  const url = "/api/v1/actuator/addactuator";
 
-    // Prepare the dataRotor to be added (usually in JSON format)
-    const data = {
-        name: "rotor",
-        state: 20
-    };
+  // Prepare the dataRotor to be added (usually in JSON format)
+  const data = {
+    name: "rotor",
+    state: 20,
+  };
 
-    // Make the POST request
-        fetch('http://localhost:4321' + url, {
-        method: 'POST',
-        headers: {
-            'Accept' : 'application/json',
-            'Content-Type' : 'application/json'
-            },
-        body: JSON.stringify({data}) // Convert data to JSON string
-        })
-        .then(response => {
-            // Check for successful response status before parsing as JSON
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.json();
-        })
-        // Now parse the JSON response
-        .then(addedData => {
-            // Handle the added data here (e.g., update UI)
-            console.log('Data added successfully:', addedData);
-        })
-        .catch(error => {
-            console.error('Error adding data:', error);
-        });
-}
+  // Make the POST request
+  fetch("http://localhost:4321" + url, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ data }), // Convert data to JSON string
+  })
+    .then((response) => {
+      // Check for successful response status before parsing as JSON
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    // Now parse the JSON response
+    .then((addedData) => {
+      // Handle the added data here (e.g., update UI)
+      console.log("Data added successfully:", addedData);
+    })
+    .catch((error) => {
+      console.error("Error adding data:", error);
+    });
+};
 
 // Fetch motor and airpump states when the page loads
-window.onload = function() {
-    addActuatorState();
+window.onload = function () {
+  addActuatorState();
 };
