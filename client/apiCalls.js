aad42d3f85881a99776ef45373540fbaf17ec6c5
MelcherMate <melchermate28@gmail.com>
2024-03-18 21:40:40 +0100

    working get call with all data from DB sent to client

--- client/apiCalls.js
+++ client/apiCalls.js
@@ -1,3 +1,27 @@
+// Function to fetch actuator states from server
+const fetchActuatorStates = async () => {
+  try {
+    // Define the URL of the API endpoint
+    const url = "/api/v1/actuator/getactuators";
+
+    // Make the GET request
+    const response = await fetch(url);
+
+    // Check for successful response status
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    // Parse the JSON response
+    const actuators = await response.json();
+
+    // Handle the retrieved actuators
+    console.log("Retrieved actuators:", actuators);
+  } catch (error) {
+    console.error("Error fetching actuators:", error);
+  }
+};
+
 // Function to fetch actuator state from server
 const addActuatorState = async (val) => {
   var output = document.getElementById("output");
@@ -5,14 +29,14 @@ const addActuatorState = async (val) => {
   // Define the URL of the API endpoint
   const url = "/api/v1/actuator/addactuator";
 
-  // Prepare the dataRotor to be added (usually in JSON format)
+  // Prepare the data to be added (usually in JSON format)
   const data = {
     name: "rotor",
     state: val,
   };
 
   // Make the POST request
-  fetch("http://localhost:4321" + url, {
+  fetch(url, {
     method: "POST",
     headers: {
       Accept: "application/json",
@@ -37,7 +61,7 @@ const addActuatorState = async (val) => {
     });
 };
 
-// Fetch motor and airpump states when the page loads
+// Fetch actuator states when the page loads
 window.onload = function () {
   var slider = document.getElementById("myRange");
   var output = document.getElementById("output");
@@ -47,4 +71,7 @@ window.onload = function () {
     output.value = this.value;
     addActuatorState(this.value);
   };
+
+  // Fetch actuator states
+  fetchActuatorStates();
 };
