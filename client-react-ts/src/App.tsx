1695f130bd23fac5eae2024a4ddfb6cba1b01820
MelcherMate <melchermate28@gmail.com>
2024-05-05 17:53:24 +0200

    linecharts added, new main, nav, aside

--- client-react-ts/src/App.tsx
+++ client-react-ts/src/App.tsx
@@ -1,4 +1,14 @@
 import { useState } from "react";
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
 import "./App.css";
 import Button from "./components/Button";
 import Slider from "./components/Slider";
@@ -11,8 +21,8 @@ function App() {
     <>
       <div className="grid">
         <header>Bioversee</header>
-        <main className="mainGrid">
-          <div className="switchContainer">
+        <div className="container">
+          <nav>
             <Switch
               name="switchWarmWaterPump"
               label="Warm water pump"
@@ -37,8 +47,6 @@ function App() {
               url="/api/v1/actuator/getswitchesactuators"
               updateUrl="/api/v1/actuator/postswitchactuator"
             />
-          </div>
-          <div className="sliderContainer">
             <Slider
               name="rotor"
               label="Rotor"
@@ -51,8 +59,9 @@ function App() {
               url="/api/v1/actuator/getslideractuators"
               updateUrl="/api/v1/actuator/postslideractuator"
             />
-          </div>
-          <div className="buttonContainer">
+          </nav>
+          <main>Hello main</main>
+          <aside>
             <Button
               name="temperature"
               label="Temperature button"
@@ -63,8 +72,68 @@ function App() {
               label="pH value button"
               url="/api/v1/sensor/getsensordata"
             />
-          </div>
-        </main>
+            <LineChart
+              width={200}
+              height={200}
+              data={[
+                {
+                  name: "Page A",
+                  pv: 2400,
+                  amt: 2400,
+                },
+                {
+                  name: "Page B",
+                  pv: 1398,
+                  amt: 2210,
+                },
+                {
+                  name: "Page C",
+                  pv: 9800,
+                  amt: 2290,
+                },
+                {
+                  name: "Page D",
+                  pv: 3908,
+                  amt: 2000,
+                },
+                {
+                  name: "Page E",
+                  pv: 4800,
+                  amt: 2181,
+                },
+                {
+                  name: "Page F",
+                  pv: 3800,
+                  amt: 2500,
+                },
+                {
+                  name: "Page G",
+                  pv: 4300,
+                  amt: 2100,
+                },
+              ]}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line
+                type="monotone"
+                dataKey="pv"
+                stroke="#8884d8"
+                activeDot={{ r: 8 }}
+              />
+              <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+            </LineChart>
+          </aside>
+        </div>
         <footer>Bioversee open-source software</footer>
       </div>
     </>
