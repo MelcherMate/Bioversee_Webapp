9e588672f60af75edbc527328efb0a1040ac0559
MelcherMate <melchermate28@gmail.com>
2024-05-18 20:52:06 +0200

    data for linecharts reach the website

--- client-react-ts/src/components/Chart/index.tsx
+++ client-react-ts/src/components/Chart/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { isUndefined } from "lodash";
+import React, { useEffect } from "react";
 import {
   CartesianGrid,
   Legend,
@@ -17,15 +18,27 @@ interface ChartProps {
 }
 
 const Chart: React.FC<ChartProps> = (props) => {
-  const data = [
-    { time: "8:00:00", pv: 22.1 },
-    { time: "8:01:00", pv: 22.4 },
-    { time: "8:02:00", pv: 18.9 },
-    { time: "8:03:00", pv: 19.9 },
-    { time: "8:04:00", pv: 21.4 },
-    { time: "8:05:00", pv: 23.5 },
-    { time: "8:06:00", pv: 24.1 },
-  ];
+  useEffect(() => {
+    if (!isUndefined(props.url) && !isUndefined(props.name)) {
+      fetch(`${import.meta.env.VITE_SERVER_BASE_URL + props.url}`, {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response Error");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          console.log("Data pull sucess:", data);
+        })
+        .catch((error) => console.error("Fetch error:", error));
+    }
+  }, [props.url, props.name]);
 
   return (
     <div className="chartContainer">
@@ -33,7 +46,16 @@ const Chart: React.FC<ChartProps> = (props) => {
       <LineChart
         width={200}
         height={200}
-        data={data}
+        data={[
+          //data
+          { time: "8:00:00", pv: 22.1 },
+          { time: "8:01:00", pv: 22.4 },
+          { time: "8:02:00", pv: 18.9 },
+          { time: "8:03:00", pv: 19.9 },
+          { time: "8:04:00", pv: 21.4 },
+          { time: "8:05:00", pv: 23.5 },
+          { time: "8:06:00", pv: 24.1 },
+        ]}
         margin={{
           top: 5,
           right: 0,
@@ -48,7 +70,7 @@ const Chart: React.FC<ChartProps> = (props) => {
         <Legend />
         <Line
           type="monotone"
-          dataKey="pv"
+          dataKey={props.name}
           stroke="#8884d8"
           activeDot={{ r: 8 }}
         />
