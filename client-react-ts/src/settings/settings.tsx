00be47197fc9b64da972d6b80d899486c3eb4e77
MelcherMate <melchermate28@gmail.com>
2024-06-05 18:12:48 +0200

    pages added

---
+++ client-react-ts/src/settings/settings.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1>Settings page</h1>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
--- client-react-ts/vite.config.ts
+++ client-react-ts/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+const root = resolve(__dirname, "src");
+const outDir = resolve(__dirname, "dist");
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root,
   plugins: [react()],
-})
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, "index.html"),
+        landing: resolve(root, "landing", "index.html"),
+        user: resolve(root, "user", "index.html"),
+        settings: resolve(root, "settings", "index.html"),
+      },
+    },
+  },
+});
