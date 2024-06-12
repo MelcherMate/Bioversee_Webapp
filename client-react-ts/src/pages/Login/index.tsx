212099f7b2b937ff0addd5521930601e0257e86c
MelcherMate <melchermate28@gmail.com>
2024-06-12 17:21:03 +0200

    login cleanup

--- client-react-ts/src/pages/Login/index.tsx
+++ client-react-ts/src/pages/Login/index.tsx
@@ -1,5 +1,3 @@
-import Facebook from "../../img/facebook.png";
-import Github from "../../img/github.png";
 import Google from "../../img/google.png";
 import "./Login.css";
 
@@ -8,35 +6,12 @@ const Login = () => {
     window.open("http://localhost:4321/auth/google", "_self");
   };
 
-  const github = () => {
-    window.open("http://localhost:5173/auth/github", "_self");
-  };
-
-  const facebook = () => {
-    window.open("http://localhost:5173/auth/facebook", "_self");
-  };
-
   return (
     <div id="loginPage">
       <div id="loginBox">
-        <div id="leftBox">
-          <div className="mainLoginButton google" onClick={google}>
-            <img src={Google} alt="" className="icon" />
-            Google
-          </div>
-          <div className="mainLoginButton facebook" onClick={facebook}>
-            <img src={Facebook} alt="" className="icon" />
-            Facebook
-          </div>
-          <div className="mainLoginButton github" onClick={github}>
-            <img src={Github} alt="" className="icon" />
-            Github
-          </div>
-        </div>
-        <div id="rightBox">
-          <input className="inputBox" type="text" placeholder="Username" />
-          <input className="inputBox" type="text" placeholder="Password" />
-          <button className="submit">Login</button>
+        <div className="mainLoginButton google" onClick={google}>
+          <img src={Google} alt="" className="icon" />
+          Google
         </div>
       </div>
     </div>
