b9f700654a6b922377f548168f588f75ac5bd406
MelcherMate <melchermate28@gmail.com>
2024-06-30 08:21:56 +0200

    backend build process added

---
+++ backend/app-compiled.js
@@ -0,0 +1,9 @@
+"use strict";
+
+require("dotenv").config();
+require("@babel/register")({
+  presets: ["@babel/preset-env"]
+});
+
+// Import the rest of our application.
+module.exports = require("./start.js");
