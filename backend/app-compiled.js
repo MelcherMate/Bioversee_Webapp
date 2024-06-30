18476adc272f953d9a97e733573c2285d7f5023a
MelcherMate <melchermate28@gmail.com>
2024-06-30 08:55:26 +0200

    pkg update

--- backend/app-compiled.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-require("dotenv").config();
-require("@babel/register")({
-  presets: ["@babel/preset-env"]
-});
-
-// Import the rest of our application.
-module.exports = require("./start.js");
