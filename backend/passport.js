4c71e223a8d2ac70b31b66dc9475e97e8ef3bde5
MelcherMate <melchermate28@gmail.com>
2024-06-30 18:47:30 +0200

    passport change

--- backend/passport.js
+++ backend/passport.js
@@ -16,7 +16,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: process.env.GOOGLE_CLIENT_CALLBACK_URI,
     },
     function (accessToken, refreshToken, profile, done) {
       done(null, profile);
