59147e18fb282093cd19837733bc704346a6de26
MelcherMate <melchermate28@gmail.com>
2024-07-01 19:38:59 +0200

    auth changes

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -38,7 +38,7 @@ router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
 router.get(
   "/google/callback",
   passport.authenticate("google", {
-    // successRedirect: process.env.CORS_ALLOWED_ORIGINS + "/dashboard",
+    successRedirect: process.env.PUBLIC_URL + "/dashboard",
     successRedirect: "/login/success",
     failureRedirect: "/login/failed",
   })
