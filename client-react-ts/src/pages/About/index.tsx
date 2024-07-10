2e600b31272c13b87f073cc73e287a7a5996241b
MelcherMate <melchermate28@gmail.com>
2024-07-10 02:53:47 +0200

    infocards created and added to About Page

--- client-react-ts/src/pages/About/index.tsx
+++ client-react-ts/src/pages/About/index.tsx
@@ -1,11 +1,24 @@
-import ConstructionPage from "../../components/ConstructionPage";
+import InfoCard from "../../components/infoCard";
+import "../../img/noBackgroundLogo.png";
 import "./About.css";
 
-function About() {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function About(props: any) {
   return (
-    <>
-      <ConstructionPage></ConstructionPage>
-    </>
+    <main className="container" id="aboutContainer">
+      <InfoCard
+        title="Founder"
+        imagePath="../../img/Mate.png"
+        subtitle="Mate Melcher"
+        content="Biochemical engineer aiming to make industrial bioprocess automation as easy as child's play"
+      />
+      <InfoCard
+        title="Co-Founder"
+        imagePath="../../img/David.png"
+        subtitle="David Kutas"
+        content="An amazing front-end developer from Silicon Valley, Mate's friend since he was born"
+      />
+    </main>
   );
 }
 
