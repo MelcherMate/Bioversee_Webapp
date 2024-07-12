9e596a0d338c96dcff36864df7873bc4b95a714a
MelcherMate <melchermate28@gmail.com>
2024-07-12 11:02:16 +0200

    about page upgraded with InfoCards and new dist forlder created

--- client-react-ts/src/pages/About/index.tsx
+++ client-react-ts/src/pages/About/index.tsx
@@ -12,11 +12,17 @@ function About(props: any) {
         subtitle="Mate Melcher"
         content="Biochemical engineer aiming to make industrial bioprocess automation as easy as child's play"
       />
-      <InfoCard
+      {/* <InfoCard
         title="Co-Founder"
         imagePath="../../img/David.png"
         subtitle="David Kutas"
         content="An amazing front-end developer from Silicon Valley, Mate's friend since he was born"
+      /> */}
+      <InfoCard
+        title="Company"
+        imagePath="../../img/noBackgroundLogo.png"
+        subtitle=""
+        content="Bioversee aims to create an affordable solution for fermentation and other bioprocess purposes as well as becoming a proper educational software for bioengineering students"
       />
     </main>
   );
