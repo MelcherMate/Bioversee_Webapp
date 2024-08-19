0c7269f471bf3bcef2c6b23765f35eaed220bb18
MelcherMate <melchermate28@gmail.com>
2024-08-19 11:19:30 +0200

    new wt_lvl sensors added to water purifier

--- client-react-ts/src/components/Canvas2/WaterPurifierCard.tsx
+++ client-react-ts/src/components/Canvas2/WaterPurifierCard.tsx
@@ -21,22 +21,42 @@ function WaterPurifierCard(props, key) {
         <div id="wrapper">
           <div className="tank" id="pufferTank">
             <p>PUFFER</p>
+            <div className="sensor wt_lvl">
+              <div className="sensor_wt_lvl_base"></div>
+              <div className="sensor_wt_lvl_head1"></div>
+              <div className="sensor_wt_lvl_head2"></div>
+            </div>
           </div>
           <div id="tubePufferToActive1"></div>
           <div id="tubePufferToActive2"></div>
           <div className="tank" id="activeTank">
             <p>ACTIVE</p>
+            <div className="sensor wt_lvl">
+              <div className="sensor_wt_lvl_base"></div>
+              <div className="sensor_wt_lvl_head1"></div>
+              <div className="sensor_wt_lvl_head2"></div>
+            </div>
           </div>
           <div id="tubeAdditiveToActive1"></div>
           <div id="tubeAdditiveToActive2"></div>
           <div id="tubeAdditiveToActive3"></div>
           <div className="tank" id="additiveTank">
             <p>ADDITIVE</p>
+            <div className="sensor wt_lvl">
+              <div className="sensor_wt_lvl_base"></div>
+              <div className="sensor_wt_lvl_head1"></div>
+              <div className="sensor_wt_lvl_head2"></div>
+            </div>
           </div>
           <div id="tubeActiveToClean1"></div>
           <div id="tubeActiveToClean2"></div>
           <div className="tank" id="cleanTank">
             <p>CLEAN</p>
+            <div className="sensor wt_lvl">
+              <div className="sensor_wt_lvl_base"></div>
+              <div className="sensor_wt_lvl_head1"></div>
+              <div className="sensor_wt_lvl_head2"></div>
+            </div>
           </div>
         </div>
       </body>
